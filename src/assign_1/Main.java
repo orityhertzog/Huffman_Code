c9f72966d603e2eb6d41c6bbc7cf2e907d9491f5
Orit Hertzog <orityhertzog@gmail.com>
2020-10-11 13:39:37 +0300

    updated the main file

--- src/assign_1/Main.java
+++ src/assign_1/Main.java
@@ -8,14 +8,15 @@ public class Main {
          String[] The_original_File={"C:\\Users\\Orit\\workspace\\Assign_1\\OnTheOrigin.txt"};
          String[] The_compress_File={"C:\\Users\\Orit\\workspace\\Assign_1\\new_compress_file.bin"};
          String[] The_decompress_file={"C:\\Users\\Orit\\workspace\\Assign_1\\new_decompress_file.txt"};
-         String[] better_compress_file={"C:\\Users\\Orit\\workspace\\Assign_1\\better_compress.bin"};
-         String[] better_decompress_file={"C:\\Users\\Orit\\workspace\\Assign_1\\better_decompress_file.txt"};
+         //String[] better_compress_file={"C:\\Users\\Orit\\workspace\\Assign_1\\better_compress.bin"};
+         //String[] better_decompress_file={"C:\\Users\\Orit\\workspace\\Assign_1\\better_decompress_file.txt"};
         	HuffmanEncoderDecoder HED=new HuffmanEncoderDecoder();
 		HED.Compress(The_original_File, The_compress_File);
 		HED.Decompress(The_compress_File, The_decompress_file);
-		HuffmanBetterEnDe j=new HuffmanBetterEnDe();
+		/*HuffmanBetterEnDe j=new HuffmanBetterEnDe();
 		j.Compress(The_original_File, better_compress_file);
 		j.Decompress(better_compress_file, better_decompress_file);
+		*/
 	}
 	
 }
